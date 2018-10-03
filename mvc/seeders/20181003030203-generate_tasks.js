425ad4b81f204df3ea00b5f1433572ebc5f3b749
Jose Antonio <j-osea-nt@hotmail.com>
2018-10-02 22:09:42 -0500

    adding seed

---
+++ mvc/seeders/20181003030203-generate_tasks.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('tasks', [
+      { id: 1, description: 'Save the backend course', createdAt: new Date(), updatedAt: new Date()},
+      { id: 2, description: 'Edit the videos of backend course', createdAt: new Date, updatedAt: new Date()},
+      { id: 3, description: 'Upload the videos', createdAt: new Date(), updatedAt: new Date() }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('tasks', null, {});
+  }
+};
